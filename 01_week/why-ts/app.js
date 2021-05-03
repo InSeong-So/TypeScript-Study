a9061c10af8e1748bc14d9cfad485024bb3d1dfd
sisparang <goflvhxj2547@gmail.com>
2021-05-03 21:45:22 +0900

    JSDOC으로 타입을 정의하여 사용하기

--- 01_week/why-ts/app.js
+++ 01_week/why-ts/app.js
@@ -9,9 +9,34 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser(){
+  return axios.get(url);
+}
+
+fetchUser.then(function(response) {
+  response.address.city;
+})
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
