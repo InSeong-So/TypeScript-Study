cf7dfc22820c3e1dc205d418cdb90beac139a7be
sisparang <goflvhxj2547@gmail.com>
2021-05-24 22:31:21 +0900

    점진적인 타입 적용 완료

--- 03_week/project/src/app.ts
+++ 03_week/project/src/app.ts
@@ -7,10 +7,12 @@ function getUnixTimestamp(date: Date) {
 }
 
 // DOM
-const confirmedTotal = $('.confirmed-total');
-const deathsTotal = $('.deaths');
-const recoveredTotal = $('.recovered');
-const lastUpdatedTime = $('.last-updated-time');
+// let element: Element | HTMLElement | HTMLParagraphElement;
+// 타입 간에 호환이 되지 않는 형태일 때 단언으로 이를 해결
+const confirmedTotal = $('.confirmed-total') as HTMLSpanElement;
+const deathsTotal = $('.deaths') as HTMLParagraphElement;
+const recoveredTotal = $('.recovered') as HTMLParagraphElement;
+const lastUpdatedTime = $('.last-updated-time') as HTMLParagraphElement;
 const rankList = $('.rank-list');
 const deathsList = $('.deaths-list');
 const recoveredList = $('.recovered-list');
